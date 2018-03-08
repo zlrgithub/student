b78e9c484767007762b66a5eef10566c8db361e3
zoulr <zoulr@asiainfo.com>
2018-03-08 10:00:40 +0800

    a

---
+++ student/src/main/java/com/student/until/WorkInfoMap.java
@@ -0,0 +1,19 @@
+package com.student.until;
+
+public class WorkInfoMap {
+	public String name;
+	public Long value;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getValue() {
+		return value;
+	}
+	public void setValue(Long value) {
+		this.value = value;
+	}
+}
